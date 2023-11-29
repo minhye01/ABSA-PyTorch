67e40485b8b662e286e35f92b9c2eda0a723f4a8
minhye01 <145265863+minhye01@users.noreply.github.com>
2023-11-29 19:32:18 +0900

    Update bert_spc.py for kobert

--- models/bert_spc.py
+++ models/bert_spc.py
@@ -15,7 +15,13 @@ class BERT_SPC(nn.Module):
 
     def forward(self, inputs):
         text_bert_indices, bert_segments_ids = inputs[0], inputs[1]
-        _, pooled_output = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
-        pooled_output = self.dropout(pooled_output)
+        
+        #여기 코드 고쳤음
+        outputs = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
+        pooled_output = outputs.pooler_output
+        #_, pooled_output = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
+        #pooled_output = self.dropout(pooled_output)
+        #여기 코드 고쳤음
+        
         logits = self.dense(pooled_output)
         return logits
